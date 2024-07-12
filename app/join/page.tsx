60d793217f061a77c70d6444b59d7f72b0f76b1b
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2024-07-12 21:21:07 +0900

    [add]join.module.css

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,9 +1,21 @@
-export default function Home() {
-    return (
-      <div className="text-center font-bold">
-        <h1>ハッカソンに参加</h1>
-      </div>
-    )
-  }
+import style from './Join.module.css'
 
-  
+export default function Join() {
+  return (
+    <div>
+      <div className={style.back}>
+        <button className={style.button}></button>
+        <div>
+          <footer>
+            <ul>
+              <li>ハッカソンレベル</li>
+              <li>日時</li>
+              <li>場所</li>
+              <li>説明</li>
+            </ul>
+          </footer>
+        </div>
+      </div>
+    </div>
+  )
+}
